cf4e85c1745a614da783a193ec9d20932ef125c0
Ajay Sarkate <115990815+ajay-s-07@users.noreply.github.com>
2023-03-18 16:45:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 //your code here
-let arr = [2,1,2];
-document.getElementById("output").innerText = arr[0];
+function majority(){
+	let max = 0, count=0;
+	for(let i=0; i<nums.length; i++){
+		if(count == 0) max = nums[i]; //  2,
+		if(nums[i] == max) count++; // 1,
+		else count--;
+	}
+	return max;
+}
