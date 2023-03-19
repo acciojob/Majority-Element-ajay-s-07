102c4bd615d3b11de98cb64810628f97ca70f113
Ajay Sarkate <115990815+ajay-s-07@users.noreply.github.com>
2023-03-19 12:57:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,21 +13,22 @@
 // majority();
 
 function majorityElementChecker(arr)
-    {
-        let map = new Map();
- 
-        for(let i = 0; i < arr.length; i++) {
-            if (map.has(arr[i])) {
-                    let count = map.get(arr[i]) +1;
-                    if (count > arr.length /2) {
-                        document.write("Majority found :- " + arr[i]);
-                        return;
-                    } else
-                        map.set(arr[i], count);
- 
-            }
-            else
-                map.set(arr[i],1);
-            }
-            document.write(" No Majority element");
-    }
+{
+	let map = new Map();
+
+	for(let i = 0; i < arr.length; i++) {
+		if (map.has(arr[i])) {
+			let count = map.get(arr[i]) +1;
+			if (count > arr.length /2) {
+				document.write("Majority found :- " + arr[i]);
+				return;
+			} else
+				map.set(arr[i], count);
+		}
+		else
+			map.set(arr[i],1);
+		}
+		document.write(" No Majority element");
+}
+let arr = [2,1,2];
+majorityElementChecker(arr);
